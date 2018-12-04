85bff9b157b18b216b8875aea69ef5fcdb5e4930
Fred Gandt <fred.gandt.aka.fg@gmail.com>
2018-12-04 19:48:36 +0000

    deminified version 2.0.1

--- RFCRestyle/popup.js
+++ RFCRestyle/popup.js
@@ -1,61 +1,104 @@
-const W = window,
-	D = document;
-W.addEventListener( "DOMContentLoaded", () => {
-	D.getElementById( "close" ).addEventListener( "click", () => W.close() );
-	chrome.storage.local.get( i => {
-		const o = D.getElementById( "options" ),
-			save = () => {
-				chrome.storage.local.set( {
-					htmlbg: { r: o.htmlbgr.value, g: o.htmlbgg.value, b: o.htmlbgb.value },
-					bodybg: { r: o.bodybgr.value, g: o.bodybgg.value, b: o.bodybgb.value },
-					slctn: { r: o.slctnr.value, g: o.slctng.value, b: o.slctnb.value },
-					nrml: { r: o.nrmlr.value, g: o.nrmlg.value, b: o.nrmlb.value },
-					lght: { r: o.lghtr.value, g: o.lghtg.value, b: o.lghtb.value },
-					lnk: { r: o.lnkr.value, g: o.lnkg.value, b: o.lnkb.value },
-					vha: { r: o.vhar.value, g: o.vhag.value, b: o.vhab.value },
-					ul: Array.from( D.querySelectorAll( "#uls input" ) ).find( n => n.checked ).value,
-					continuous: o.continuous.checked.valueOf(),
-					rdrct: o.rdrct.checked.valueOf(),
-					fzzy: o.fzzy.checked.valueOf(),
-					styl: o.styl.checked.valueOf(),
-					fntsz: o.fntsz.value
-				} );
-			};
-		o.addEventListener( "click", e => {
-			let t = e.target,
-				tt = t.type;
-			if ( tt && tt === "button" ) {
-				o.setAttribute( "class", t.parentElement.id );
-			}
+"use strict";
+const A = document,
+	B = "input",
+	C = "radio",
+	D = "name",
+	E = "type",
+	F = "value",
+	G = "checked",
+	H = "label",
+	I = "addEventListener",
+	J = "[" + E + '="' + C + '"]',
+	K = "prfl",
+	L = K + "s",
+	O = " Mono",
+	P = "appendChild",
+	Q = "querySelector",
+	R = "forEach",
+	S = chrome.storage.local,
+	T = a => A[ Q ]( a ),
+	U =( b, c ) => {
+		let d = A.createElement( c ? "option" : b );
+		if ( c ) {
+			d.textContent = b;
+		}
+		return d;
+	},
+	V = ( e, f ) => ~f.indexOf( e[ E ] ),
+	W = T( "form" ),
+	X = g => W.stylrs.disabled = !g.styl,
+	Y = h => {
+		W.sync[ G ] = h.sync;
+		h = h[ L ][ h[ K ] ];
+		X( h );
+		Object.getOwnPropertyNames( h )[ R ]( n => {
+			let i = h[ n ];
+			W[ n ][ typeof i === "boolean" ? G : F ] = i;
 		} );
-		o.addEventListener( "input", save );
-		o.addEventListener( "change", save );
-		o.continuous.checked = i.continuous;
-		o.htmlbgr.value = i.htmlbg.r;
-		o.htmlbgg.value = i.htmlbg.g;
-		o.htmlbgb.value = i.htmlbg.b;
-		o.bodybgr.value = i.bodybg.r;
-		o.bodybgg.value = i.bodybg.g;
-		o.bodybgb.value = i.bodybg.b;
-		o.slctnr.value = i.slctn.r;
-		o.slctng.value = i.slctn.g;
-		o.slctnb.value = i.slctn.b;
-		o.rdrct.checked = i.rdrct;
-		o.nrmlr.value = i.nrml.r;
-		o.nrmlg.value = i.nrml.g;
-		o.nrmlb.value = i.nrml.b;
-		o.lghtr.value = i.lght.r;
-		o.lghtg.value = i.lght.g;
-		o.lghtb.value = i.lght.b;
-		o.fntsz.value = i.fntsz;
-		o.styl.checked = i.styl;
-		o.fzzy.checked = i.fzzy;
-		o.lnkr.value = i.lnk.r;
-		o.lnkg.value = i.lnk.g;
-		o.lnkb.value = i.lnk.b;
-		o.vhar.value = i.vha.r;
-		o.vhag.value = i.vha.g;
-		o.vhab.value = i.vha.b;
-		D.getElementById( i.ul ).checked = true;
+	};
+chrome.runtime.connect( { name: "popup" } );
+[ "System default", "Lucida Console", "Consolas", "Courier",
+	[ "Inconsolata",
+		"Cousine",
+		"Nanum Gothic Coding",
+		"Source Code Pro",
+		"Anonymous Pro",
+		"Overpass" + O,
+		"IBM Plex" + O,
+		"Roboto" + O,
+		"Ubuntu" + O,
+		"Space" + O,
+		"Fira" + O
+	]
+][ R ]( ( j, k ) => {
+	let l;
+	if ( Array.isArray( j ) ) {
+		l = U( "optgroup" );
+		l[ H ] = "Google Fonts";
+		j[ R ]( m => {
+			l[ P ]( U( m, !0 ) )
+		} );
+	} else {
+		l = U( j, !0 );
+		l[ F ] = k ? j : "monospace";
+	}
+	W.fnt[ P ]( l );
+} );
+[ [ "none", "solid", "dashed", "dotted" ],
+	[ "Foo", "Bar", "Baz", "Qux", "Quux" ]
+][ R ]( ( n, o ) => {
+	n[ R ]( p => {
+		let q = o ? K : "ul",
+			r = U( H ),
+			s = U( B );
+		s[ F ] = p;
+		s[ E ] = C;
+		s[ D ] = q;
+		r[ P ]( A.createTextNode( p ) );
+		r[ P ]( s );
+		W[ q + "s" ][ P ]( r );
+	} );
+} );
+T( B )[ I ]( "click", t => self.close() );
+S.get( u => {
+	Y( u );
+	W[ I ] ( B, v => {
+		let w = { fnt: W.fnt[ F ] };
+		W[ Q + "All" ]( B + ':not([name="sync"]):not(' + J + ")," + B + J + ":" + G )[ R ]( x => {
+			w[ x[ D ] ] = V( x, [ "color", C ] ) ? x[ F ] : V( x, "range" ) ? x.valueAsNumber : x[ G ];
+		} );
+		u.sync = W.sync[ G ];
+		let y = w[ K ];
+		u[ K ] = y;
+		if ( v.target[ D ] === K ) {
+			if ( !u[ L ][ y ] ) {
+				return;
+			}
+			Y( u );
+		} else {
+			u[ L ][ y ] = w;
+			X( w );
+		}
+		S.set( u );
 	} );
 } );
