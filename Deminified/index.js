c35bde0c81a793b4fc74124e02bb0d0a4f255034
Fred Gandt <fred.gandt.aka.fg@gmail.com>
2019-03-11 13:50:19 +0000

    added fully deminified and replaced expanded code

---
+++ Deminified/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const PRE = document.querySelector( "pre" );
+
+PRE.style.cssText = "width:80ch;margin:0 auto 2em;";
+PRE.innerHTML = "<p>" + PRE.textContent.split( /\n{2,}(?!\n\n)+/mg ).join( "</p><p>" ) + "</p>";
+
+const ALL_PS = Array.from( document.querySelectorAll( "p" ) ),
+	linkify = () => {
+		ALL_PS.filter( p => {
+			let pos = p.offsetTop - document.body.scrollTop;
+			return !p.dataset.d && pos > 0 && pos < window.innerHeight;
+		} ).forEach( p => {
+			p.innerHTML = p.textContent.replace( /(?:^| (RFC|BCP))(0*([0-9]+))/g, ( mtch, grp1, grp2, grp3 ) => {
+				return ( grp1 ? " " : "" ) + '<a href="https://tools.ietf.org/html/' +
+					( grp1 ? grp1.toLowerCase() : ( /bcp-index/.test( location.pathname ) ? "bcp" : "rfc" ) )
+					+ grp3 + '" target="_blank">' + ( grp1 || "" ) + grp2 + '</a>'
+			} );
+			p.dataset.d = !0;
+		} );
+	};
+
+linkify();
+
+window.addEventListener( "scroll", linkify );
