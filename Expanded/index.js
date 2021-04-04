8cf3e08f3630c0dbf8733d55a43ca73cbce435e7
FredGandt <fred.gandt.aka.fg@gmail.com>
2021-04-04 14:53:11 +0100

    fixed fixes

--- Expanded/index.js
+++ Expanded/index.js
@@ -1,16 +1,17 @@
 "use strict";
 const D = document,
-	SE = D.scrollingElement,
+	N = location.pathname,
 	P = D.querySelector( "pre" );
 P.style.cssText = "width:80ch;margin:0 auto 2em;";
 P.innerHTML = "<p>" + P.textContent.split( /\n{2,}(?!\n\n)+/mg ).join( "</p><p>" ) + "</p>";
-const S = Array.from( D.querySelectorAll( "p" ) ),
+const SE = D.scrollingElement,
+	S = Array.from( D.querySelectorAll( "p" ) ),
 	G = v => {
 		S.filter( e => {
 			let pos = e.offsetTop - SE.scrollTop;
 			return !e.dataset.d && pos > 0 && pos < window.innerHeight;
 		} ).forEach( e => {
-			e.innerHTML = e.textContent.replace( /(?:^| (RFC|BCP))(0*([0-9]+))/g, ( m, a, b, c ) => ( a ? " " : "" ) + '<a href="https://tools.ietf.org/html/' + ( a ? a.toLowerCase() : ( /bcp-index/.test( location.pathname ) ? "bcp" : "rfc" ) ) + c + '" target="_blank">' + ( a || "" ) + b + '</a>' );
+			e.innerHTML = e.textContent.replace( /(?:^| (RFC|BCP))(0*([0-9]+))/g, ( m, a, b, c ) => ( a ? " " : "" ) + '<a href="https://tools.ietf.org/html/' + ( a ? a.toLowerCase() : ( /bcp-index/.test( N ) ? "bcp" : "rfc" ) ) + c + '" target="_blank">' + ( a || "" ) + b + '</a>' );
 			e.dataset.d = !0;
 		} );
 	};
